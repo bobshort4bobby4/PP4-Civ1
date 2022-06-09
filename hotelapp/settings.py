8d93db4476a843d6d6396ea59c72d8f618c2d313
bobshort4bobby4 <treborken@gmail.com>
2022-06-09 22:19:36 +0000

    ADD session settings

--- hotelapp/settings.py
+++ hotelapp/settings.py
@@ -32,7 +32,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ["pp4-civ1.herokuapp.com", "localhost"]
@@ -200,3 +200,6 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_COOKIE_AGE = 60 * 15
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
